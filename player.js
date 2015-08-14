31b1c5d67d6c9759af0e83d4fdc1ed0fdf344c66
Sahil Badla <sahilbadla@Ericssons-MacBook-Pro-7.local>
2015-08-13 22:55:36 -0700

    delay

--- player.js
+++ player.js
@@ -280,7 +280,7 @@ sampleplayer.CastPlayer = function(element) {
   //this.mediaManager_.onLoad = this.onLoad_.bind(this);
   this.mediaManager_.onLoad = setTimeout(function(){
   this.onLoad_.bind(this);}, 4000);
-  }
+
 
   /**
    * The original editTracksInfo callback
