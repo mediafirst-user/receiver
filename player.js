8f1e2d9aca3a9037d6cb686f7777c3a24d73f572
Sahil Badla <sahilbadla@gax1carbon.northamerica.corp.microsoft.com>
2015-08-14 16:23:57 -0700

    timeout

--- player.js
+++ player.js
@@ -276,12 +276,13 @@ sampleplayer.CastPlayer = function(element) {
    * @private {?function(cast.receiver.MediaManager.Event)}
    */
 
-// this.onLoadOrig_ = setTimeout(
-//           this.mediaManager_.onLoad.bind(this.mediaManager_), 4000);
-  this.onLoadOrig_ =
-     this.mediaManager_.onLoad.bind(this.mediaManager_);
-  //this.mediaManager_.onLoad = this.onLoad_.bind(this);
-  setTimeout(()=> this.onLoad_.bind(this), 4000);
+ this.onLoadOrig_ = setTimeout(
+           this.mediaManager_.onLoad.bind(this.mediaManager_), 4000);
+//  this.onLoadOrig_ =
+//     this.mediaManager_.onLoad.bind(this.mediaManager_);
+  this.mediaManager_.onLoad = this.onLoad_.bind(this);
+//  setTimeout(function(){
+//  this.mediaManager_.onLoad = this.onLoad_.bind(this), 4000);
 
 
   /**
