fa09f661935e1a08d46f2260b4b1616c5e58601e
Sahil Badla <sahilbadla@Ericssons-MacBook-Pro-7.local>
2015-08-13 23:00:07 -0700

    delay

--- player.js
+++ player.js
@@ -278,8 +278,8 @@ sampleplayer.CastPlayer = function(element) {
   this.onLoadOrig_ =
       this.mediaManager_.onLoad.bind(this.mediaManager_);
   //this.mediaManager_.onLoad = this.onLoad_.bind(this);
-  this.mediaManager_.onLoad = setTimeout(function(){
-  this.onLoad_.bind(this)}, 4000);
+  setTimeout(function(){
+  this.mediaManager_.onLoad = this.onLoad_.bind(this);}, 4000);
 
 
   /**
